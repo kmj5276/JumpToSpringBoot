09c57d48ae4f90538a6e0418f47158fa3055330a
kmj5276 <myeongjae1235@gmail.com>
2025-09-19 21:41:20 +0900

    3-08 글쓴이 항목 추가하기

--- src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.answer;
 import com.mysite.sbb.question.Question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import com.mysite.sbb.user.SiteUser;
 
 import java.time.LocalDateTime;
 
@@ -13,11 +14,12 @@ public class AnswerService {
     private final AnswerRepository answerRepository;
 
 
-    public void create(Question question, String content) {
+    public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
+        answer.setAuthor(author);
         this.answerRepository.save(answer);
     }
 }
